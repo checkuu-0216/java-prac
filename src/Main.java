ac73838f1a08db8d527199519169d0c76dd9d821
checkuu-0216 <corb37@gmail.com>
2024-07-22 21:38:20 +0900

    "형변환 테스트"

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 
-//import = 수입하다. / 외부에서 제공하는 것들을 가져올때 쓴다
-// 문자 -> 숫자
-
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+//       double 형 or float 형 -> int
+//        실수 -> 정수
+double doubleNumber = 10.101010;
+float floatNumber = 10.1010f;
+//변환(int)
+        int intNumber;
 
-        char letter = sc.nextLine().charAt(0); // 첫번째 글자만 받아오기위해 charAt(0) 메서드를 사용합니다.
-        int asciiNumber = (int)letter; // 숫자로 형변환을 해주면 저장되어있던 아스키 숫자값으로 표현됩니다.
+        intNumber = (int)doubleNumber;
+        System.out.println("Double type " + doubleNumber);
+        System.out.println("int type " + intNumber);
 
-        System.out.println(asciiNumber);
-    }
+            }
 
 }
