bbf63222da9d44adb5e0f23cb9a4f8313e3966be
checkuu-0216 <corb37@gmail.com>
2024-07-22 19:47:59 +0900

    "boolean 선언 테스트"

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
         // boolean 변수 선언
         //타입이름 = 값;
 
-        boolean a = true;
+       boolean flag = true;
+       flag = false;
+
+       System.out.println(flag);
 
     }
 }
