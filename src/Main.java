db108126661cd78e162fe89bf402e361317ae7bd
checkuu-0216 <corb37@gmail.com>
2024-07-22 19:42:01 +0900

    "boolean 선언"

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(7);
-        System.out.println(3);
-        System.out.println(3.14);
-        System.out.println("JAVA");
+        // 우리의 공간
+        // boolean 변수 선언
+        //타입이름 = 값;
+
+//     boolean flag = true;
+        boolean flag;
+        flag = true;
     }
 }
