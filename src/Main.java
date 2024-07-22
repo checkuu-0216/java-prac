cede08d8843dc4c7eec8db6405b091b132f27122
checkuu-0216 <corb37@gmail.com>
2024-07-22 20:20:45 +0900

    "boolean 선언 테스트"

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
         // boolean 변수 선언
         //타입이름 = 값;
 
-       boolean flag = true;
-       flag = false;
+//        char alphabet = "A"; char에 ""쌍따옴표로 선언할경우 문자열로 인식해 오류난다
+        char alphabet = 'A';
 
-       System.out.println(flag);
-
+//        sout 이라고 하면 intellij에서 System.out.println이 탭으로 나온다
+        System.out.println(alphabet);
     }
 }
