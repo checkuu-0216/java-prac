08c5d4efd5a5b4ce42ce4d1fdf2e29f50d197c16
checkuu-0216 <corb37@gmail.com>
2024-07-22 20:32:37 +0900

    "참조형 string 테스트"

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ public class Main {
         // boolean 변수 선언
         //타입이름 = 값;
 
-// 실수형 float(4byte), double(8byte)
-        float floatNumber = 0.123F;
-        double doubleNumber = 0.123123132;
-
-        System.out.println(floatNumber);
-        System.out.println(doubleNumber);
+// 참조형 문자열 변수
+        String helloWorld = "Hello world!";
+// 생성 후 가져 온다 참조형
+        System.out.println(helloWorld);
     }
 }
