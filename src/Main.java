848866aa426644d3ec3622a1aba6190a83030ebc
checkuu-0216 <corb37@gmail.com>
2024-07-22 20:35:11 +0900

    "참조형 배열 arrays 테스트"

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -5,9 +6,9 @@ public class Main {
         // boolean 변수 선언
         //타입이름 = 값;
 
-// 참조형 문자열 변수
-        String helloWorld = "Hello world!";
-// 생성 후 가져 온다 참조형
-        System.out.println(helloWorld);
+// 참조형 배열
+        int[] a ={1, 2, 3};
+//참조형 데이터는 주소 값을 대입한다.
+        System.out.println(Arrays.toString(a));
     }
 }
