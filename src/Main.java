c6cf3ac4212838179964ee76868ef63da7b479cd
checkuu-0216 <corb37@gmail.com>
2024-07-22 21:13:21 +0900

    "ascii 문자에서 숫자 변환 테스트"

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
-import java.util.Arrays;
+
+//import = 수입하다. / 외부에서 제공하는 것들을 가져올때 쓴다
+// 문자 -> 숫자
+
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        // 우리의 공간
-        // boolean 변수 선언
-        //타입이름 = 값;
+        Scanner sc = new Scanner(System.in);
 
-// 래퍼 클래스 (Wrapper class 변수)
-        int number = 21;
-        Integer num = number; // boxing
+        char letter = sc.nextLine().charAt(0); // 첫번째 글자만 받아오기위해 charAt(0) 메서드를 사용합니다.
+        int asciiNumber = (int)letter; // 숫자로 형변환을 해주면 저장되어있던 아스키 숫자값으로 표현됩니다.
 
-//        console.log(num.intValue())
-        System.out.println(num.intValue()); //unboxing
+        System.out.println(asciiNumber);
     }
+
 }
