797d9085a7173684dc14f085183a3047c227b687
checkuu-0216 <corb37@gmail.com>
2024-07-22 20:28:30 +0900

    "정수형 (byte, short, int, long) 테스트"

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,17 @@ public class Main {
         // boolean 변수 선언
         //타입이름 = 값;
 
-//        char alphabet = "A"; char에 ""쌍따옴표로 선언할경우 문자열로 인식해 오류난다
-        char alphabet = 'A';
+//        정수형 (byte, short, int, long)
 
-//        sout 이라고 하면 intellij에서 System.out.println이 탭으로 나온다
-        System.out.println(alphabet); 
+        byte bytenumber = 127; // -128 ~127(1byte) 표현범위 넘어가는 숫자를 입력하면 오류난다 ex)300
+        //        카멜 케이스
+        short shortnumber = 32767; // -32,768 ~ 32,767
+        int intnumber = 2147483647;
+        long longnumber = 2147483647L;
+
+        System.out.println(bytenumber);
+        System.out.println(shortnumber);
+        System.out.println(intnumber);
+        System.out.println(longnumber);
     }
 }
